2881050d5bc9fb57bf02d721e113b4de277b9385
Evan Taylor <77093543+Evan031@users.noreply.github.com>
2023-02-03 10:46:27 +0200

    Started task 18

--- task-18/app.js
+++ task-18/app.js
@@ -3,26 +3,46 @@ const app = express()
 const fileHandler = require('fs');
 
 
-app.get('/', function(req, res) {
+app.get('/api', function(req, res) {
     fileHandler.readFile('projects.json', (err, data) => {
         if (err) res.send('File not found. First post to create file.');
         else
-            res.send(`Hello World! ${data}`);
+            res.send(`${data}`);
     })
 })
-app.get('/about', function(req, res) {
-    fileHandler.readFile('person.json', (err, data) => {
-        if (err) res.send('File not found. First post to create file.');
+
+app.post('/add', (req, res) => {
+    const reqData = req.body;
+    // I scheme what could work taking the data and pushing the reqData on to it
+    fileHandler.readFile('projects.json', (err, data) => {
+        if (err) res.send('File not found.');
         else
-            res.send(`I'm the about page! ${data}`);
+            res.send(`${data}`);
     })
+    // fileHandler.writeFile('person.json', `{"name": "${req.query.name}"}`, (err, data) => {
+    //     if (err) throw err;
+    //     res.send('File created!');
+    // });
 })
-app.post('/', (req, res) => {
-    fileHandler.writeFile('person.json', `{"name": "${req.query.name}"}`, (err) => {
-        if (err) throw err;
-        res.send('File created!');
-    });
-})
+
+// router.post("/my-endpoint", (req, res) => {
+//     // Extract the data from the request body
+//     const data = req.body;
+  
+//     // Perform any necessary data validation
+//     if (!data.name) {
+//       return res.status(400).send("Name is required");
+//     }
+  
+//     // Perform the task associated with the post request
+//     // e.g. Save data to the database
+  
+//     // Return the appropriate response to the client
+//     return res.send("Data saved successfully");
+//   });
+  
+//   module.exports = router;
+
 app.delete('/', (req, res) => {
     fileHandler.unlink('person.json', (err) => {
         if (err) res.send('File not found. First post to create file.');
@@ -30,6 +50,7 @@ app.delete('/', (req, res) => {
             res.send('File deleted!');
     });
 })
+
 app.put('/:name', (req, res) => {
     fileHandler.writeFile('person.json', `{"name": "${req.params.name}"}`, (err) => {
         if (err) throw err;
